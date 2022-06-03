e2a48a15f58eb2b4e209941a79478cb48b123eb4
Bruce Alberto <brucealberto2@gmail.com>
2022-06-03 15:14:00 -0300

    Requisito 01

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
 const express = require('express');
 const cors = require('cors');
+require('dotenv').config();
 
 const strangerThingsDataset = require('./data/dataset/stranger-things-characters.json');
 const StrangerThingsRepository = require('./data/repository/StrangerThings');
 const StrangerThingsService = require('./services/StrangerThings');
 
 const app = express();
+const PORT = process.env.PORT || 3000;
+// Fonte : https://pt.stackoverflow.com/questions/235272/como-converter-uma-string-em-booleano
+const stringBool = process.env.UPSIDEDOWN_MODE;
+const parseBool = stringBool.toLocaleLowerCase() === 'true';
 
 const strangerThingsRepository = new StrangerThingsRepository(
   strangerThingsDataset,
@@ -16,7 +21,7 @@ const strangerThingsService = new StrangerThingsService(
 
 app.use(cors());
 
-const hereIsTheUpsideDown = true;
+const hereIsTheUpsideDown = parseBool;
 
 app.get('/', (req, res) => {
   const characters = strangerThingsService.search(
@@ -27,6 +32,6 @@ app.get('/', (req, res) => {
   res.status(200).json(characters);
 });
 
-app.listen(3000, () => {
+app.listen(PORT, () => {
   console.log('Escutando na porta 3000');
 });
